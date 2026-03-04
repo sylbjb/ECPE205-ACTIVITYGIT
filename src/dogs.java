cef25e10a4afbef56916cd1cf9d12b7db1f0243f
CHRISTOPHER RIGGS MERCURIO DEL CANO <2402921@cc.usls.edu>
2026-03-04 15:31:16 +0800

    added more woofs

--- src/dogs.java
+++ src/dogs.java
@@ -3,6 +3,12 @@ public class dogs {
     //HELLO DOGS
     static void main() {
         System.out.println("WOOF WOOF");
+        System.out.println("AWW WOOF");
+        System.out.println("WOOF WOOF");
+        System.out.println("WOOF WOOF");
+        System.out.println("WOOF WOOF");
+        System.out.println("WOOF WOOF");
+        System.out.println("WOOF WOOF");
     }
 
 }
